28c530a60d7295126364b6f9f633dcd1b1e1624b
Matt Radford <quinquex@gmail.com>
2025-02-03 09:45:19 -0800

    add navbar

--- js/snkrx-ish.js
+++ js/snkrx-ish.js
@@ -13,3 +13,23 @@
 
 
 /*----------- Event Listeners ----------*/
+
+const audioOn = document.getElementById('sound-on');
+const audioOff = document.getElementById('sound-off');
+let audioMuted = false;
+
+[audioOn, audioOff].forEach(b => b.addEventListener('click', (e) => {
+    if (audioMuted) {
+        audioOn.style.filter = 'invert(100%)';
+        audioOff.style.filter = 'invert(30%)';
+        console.log('sound on');
+        audioMuted = false;
+    } else {
+        audioOff.style.filter = 'invert(100%)';
+        audioOn.style.filter = 'invert(30%)';
+        console.log('sound off');
+        audioMuted = true;
+    };
+    
+    
+}));
