7b3155d1696d8ca90647688083f20e0da93eb81e
Matt Radford <quinquex@gmail.com>
2025-02-06 08:20:25 -0800

    audio take 3

--- js/data.js
+++ js/data.js
@@ -36,7 +36,8 @@ const bgAudio = [bgAudio1, bgAudio2, bgAudio3, bgAudio4];
 let bgIndex = bgAudio.length - 1;
 
 // const wallAudio = new Audio('../audio/wall-bounce.mp3');
-const wallAudio = new Audio('https://github.com/radford-coding/snkrx-inspired-browser-game/blob/main/audio/wall-bounce.mp3');
+// const wallAudio = new Audio('https://radford-coding.github.io/audio/wall-bounce.mp3');
+const wallAudio = new Audio('https://github.com/radford-coding/snkrx-inspired-browser-game/blob/main/audio/wall-bounce.mp3?raw=true');
 const killAudio = new Audio('../audio/kill.mp3');
 const dmgAudio = new Audio('../audio/dmg.mp3');
 
